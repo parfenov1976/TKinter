3fab877b5aca998dfed552c7a412bb3340993eba
parfenov1976 <parfenov1976@mail.ru>
2022-01-21 21:38:55 +0300

    21.01.2021

--- 15_text_editor.py
+++ 15_text_editor.py
@@ -31,15 +31,17 @@ window.title("Простой текстовый редактор")
 window.rowconfigure(0, minsize=600, weight=1)  # установки минимальной высоты строки 800 пикселей
 window.columnconfigure(1, minsize=800, weight=1)  # установки минимальной ширины столбца 800 пикселей
 
-txt_edit = tk.Text(window)  # создание текстового поля в окне
+sb_vertical = tk.Scrollbar(window, orient=tk.VERTICAL)  # добавление вертикальной полосы прокрутки
+txt_edit = tk.Text(window, yscrollcommand=sb_vertical.set)  # создание текстового поля в окне и связывание с прокруткой
+sb_vertical.config(command=txt_edit.yview)  # определение действия для полосы прокрутки
 fr_buttons = tk.Frame(window)  # создание фрейма кнопок в окне
 btn_open = tk.Button(fr_buttons, text="Открыть", command=open_file)  # создание кнопки в фрейме кнопок
 btn_save = tk.Button(fr_buttons, text="Сохранить как...", command=save_file)  # создание кнопки в фрейме кнопок
 
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)  # создание сетки для фрейма кнопок и размещение кнопки
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)  # создание сетки для фрейма кнопок и размещение кнопки
-
 fr_buttons.grid(row=0, column=0, sticky="ns")  # создание сетки для окна и размещение фрейма кнопок в окне
 txt_edit.grid(row=0, column=1, sticky="nsew")  # создание сетки для окна и размещение текстового поля в окне
+sb_vertical.grid(row=0, column=2, sticky="ns")  # создание столбца сетки и размещение полосы прокрутки
 
 window.mainloop()
