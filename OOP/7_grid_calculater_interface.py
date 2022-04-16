7c017556a2fee81266b7ca0c7aa4350cd15eee9e
parfenov1976 <parfenov1976@mail.ru>
2022-04-16 20:01:45 +0300

    16.04.2022 working version

--- OOP/7_grid_calculater_interface.py
+++ OOP/7_grid_calculater_interface.py
@@ -1,9 +1,18 @@
+"""
+Примитивный калькулятор с применением встроенного фреймворка для построения
+пользовательского интерфейса Tkinter
+"""
+
 import tkinter as tk
 import tkinter.ttk as ttk
 
 
 class Example(ttk.Frame):
     def __init__(self, parent):
+        """
+        Конструктов приложения с инициализацией метода построения интерфейса.
+        :param parent: ссылка на родительское окно приложения
+        """
         ttk.Frame.__init__(self, parent)  # вызов конструктора родительского класса
         self.parent = parent  # сохранение ссылки на основное окно
         self.entry = ttk.Entry(self, font=('serif', 12), justify=tk.RIGHT)  # создание текстового однострочного поля
@@ -15,9 +24,12 @@ class Example(ttk.Frame):
                             '*': lambda x, y: x * y,
                             '+': lambda x, y: x + y,
                             '-': lambda x, y: x - y}
-        self.init_ui()  # инициализация интерфейса
+        self.init_ui()  # инициализация метода построения интерфейса
 
     def init_ui(self):
+        """
+        Метод построения интерфейса калькулятора.
+        """
         self.parent.title('Калькулятор на Tkinter')  # установка имени окна
         ttk.Style().configure('TButton', padding=(0, 5, 0, 5), font=('serif', 12))
         ttk.Style().configure('TEntry', padding=(0, 5, 0, 5))
@@ -77,44 +89,89 @@ class Example(ttk.Frame):
         self.pack()
 
     def enter_1(self):
+        """
+        Переадресация вызова на метод ввода цифры с передачей ее в виде аргумента.
+        """
         self.enter_digit('1')
 
     def enter_2(self):
+        """
+        Переадресация вызова на метод ввода цифры с передачей ее в виде аргумента.
+        """
         self.enter_digit('2')
 
     def enter_3(self):
+        """
+        Переадресация вызова на метод ввода цифры с передачей ее в виде аргумента.
+        """
         self.enter_digit('3')
 
     def enter_4(self):
+        """
+        Переадресация вызова на метод ввода цифры с передачей ее в виде аргумента.
+        """
         self.enter_digit('4')
 
     def enter_5(self):
+        """
+        Переадресация вызова на метод ввода цифры с передачей ее в виде аргумента.
+        """
         self.enter_digit('5')
 
     def enter_6(self):
+        """
+        Переадресация вызова на метод ввода цифры с передачей ее в виде аргумента.
+        """
         self.enter_digit('6')
 
     def enter_7(self):
+        """
+        Переадресация вызова на метод ввода цифры с передачей ее в виде аргумента.
+        """
         self.enter_digit('7')
 
     def enter_8(self):
+        """
+        Переадресация вызова на метод ввода цифры с передачей ее в виде аргумента.
+        """
         self.enter_digit('8')
 
     def enter_9(self):
+        """
+        Переадресация вызова на метод ввода цифры с передачей ее в виде аргумента.
+        """
         self.enter_digit('9')
 
     def enter_0(self):
+        """
+        Переадресация вызова на метод ввода цифры с передачей ее в виде аргумента.
+        """
         self.enter_digit('0')
 
     def neg_pos(self):
-        if not self.entry.get():
+        """
+        Метод установки знака числа.
+        """
+        if self.entry.get() == 'Error' or self.entry.get() == '0' or not self.entry.get():  # блокирование ввода если имеется ошибка
             return
-        if self.entry.get()[0] == '-':
+        elif self.entry.get()[0] == '-':
             self.entry.delete(0)
         else:
             self.entry.insert(0, '-')
 
+    def cls(self):
+        """
+        Метод очистки экрана
+        """
+        self.entry.delete(0, tk.END)
+        self.clear_screen = False
+
     def enter_dot(self):
+        """
+        Метод ввода десятичной точки
+        """
+        if self.clear_screen:
+            self.cls()
         if '.' in self.entry.get():
             return
         elif self.entry.get():
@@ -123,57 +180,97 @@ class Example(ttk.Frame):
             self.entry.insert(tk.END, '0.')
 
     def clear(self):
+        """
+        Метод очистки калькулятора
+        """
         self.entry.delete(0, tk.END)  # очистка экрана
         self.entry.insert(tk.END, '0')  # базовый ноль на экране
         self.memory = 0  # очистка памяти
         self.action = ''  # очистка указателя операций
 
     def delete(self):
+        """
+        Метод удаления последнего символа на экране
+        """
+        if self.clear_screen:
+            self.cls()
         self.entry.delete(len(self.entry.get()) - 1)  # удаление последнего символа
+        if not self.entry.get():
+            self.entry.insert(tk.END, '0')
 
     def division(self):
+        """
+        Переадресация вызова на метод ввода действия с передачей его в виде аргумента.
+        """
         self.actions('/')
 
     def multiplication(self):
+        """
+        Переадресация вызова на метод ввода действия с передачей его в виде аргумента.
+        """
         self.actions('*')
 
     def subtraction(self):
+        """
+        Переадресация вызова на метод ввода действия с передачей его в виде аргумента.
+        """
         self.actions('-')
 
     def addition(self):
+        """
+        Переадресация вызова на метод ввода действия с передачей его в виде аргумента.
+        """
         self.actions('+')
 
     def equally(self):
+        """
+        Переадресация вызова на метод ввода действия равно.
+        """
         self.actions()
 
-    def enter_digit(self, digit):  # FIXME ошибка при вводе, когда self.action пустой не происходит очистка экрана
+    def enter_digit(self, digit: str):
+        """
+        Метод ввода цифры
+        :param digit: текст
+        """
         if self.entry.get() == '0' or self.clear_screen:
-            self.entry.delete(0, tk.END)
-            self.clear_screen = False
-        if digit != '0':
-            self.entry.insert(tk.END, digit)
-        elif self.entry.get():
-            self.entry.insert(tk.END, '0')
+            self.cls()
+        self.entry.insert(tk.END, digit)
 
-    def actions(self, op=''):
-        if self.action:
-            self.memory = self.action_case[self.action](self.memory, float(self.entry.get()))
-            if self.memory % 1 == 0:
-                self.memory = int(self.memory)
-            self.entry.delete(0, tk.END)
-            self.entry.insert(tk.END, self.memory)
+    def actions(self, op: str = ''):
+        """
+        Метод выполнения действий.
+        :param op: текст - знак действия -,+,/,*
+        """
+        try:
+            if self.entry.get() == 'Error' or self.entry.get()[-1] == '.':
+                return
+            elif self.action:
+                self.memory = self.action_case[self.action](self.memory, float(self.entry.get()))
+                if self.memory % 1 == 0:
+                    self.memory = int(self.memory)
+                self.cls()
+                self.entry.insert(0, self.memory)
+                self.action = op
+                self.clear_screen = True
+                return
             self.action = op
+            self.memory = float(self.entry.get())
             self.clear_screen = True
-            return
-        self.action = op
-        self.memory = float(self.entry.get())
-        self.clear_screen = True
+        except (ZeroDivisionError, ValueError):
+            self.cls()
+            self.entry.insert(tk.END, 'Error')
+            self.action = ''
+            self.clear_screen = True
 
 
 def main():
-    root = tk.Tk()
-    Example(root)
-    root.mainloop()
+    """
+    Функция запуска основного цикла приложения
+    """
+    root = tk.Tk()  # создание главного окна приложения
+    Example(root)  # создание экземпляра класса приложения
+    root.mainloop()  # запуск основного цикла приложения
 
 
 if __name__ == '__main__':
